002a40b2852782b0c92b810d248bfbd4a5ddb8dc
Anmol Shah <anmolshah.20k@gmai.com>
2023-12-01 00:50:52 +0530

    display and size ADDED

--- genericTree/GenericTree.java
+++ genericTree/GenericTree.java
@@ -17,28 +17,51 @@ public class GenericTree {
         public void add(Node n){
             this.children.add(n);
         }
+
+
     }
 
+    public static void display(Node root){
+        String str = root.data + " -> ";
+        for(Node n : root.children){
+            str+= n.data + " ";
+        }
+        str += " . ";
+        System.out.println(str);
+        for(Node n : root.children){
+            display(n);
+        }
+    }
 
+    public static int size(Node node){
+        int size = 1;
+        for(Node child : node.children){
+            size += size(child);
+        }
+        return size;
+    }
 
     public static void main(String[] args) {
         int[] eulerPath = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-        Node root;
+        Node root = null;
         Stack<Node> stack = new Stack<>();
 
         for(int element : eulerPath){
             if(element != -1){
                 Node node = new Node(element);
-                stack.push(node);
                 if(stack.size() == 0){
                     root = node;
                 }else{
                     stack.peek().add(node);
                 }
+                stack.push(node);
             }else{
                 stack.pop();
             }
         }
+
+        display(root);
+        System.out.println("Size of tree -> "+size(root));
     }
 
 }
